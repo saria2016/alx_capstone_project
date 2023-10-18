f26a80d2d0a93ec78eb9879d947ccc692bf9dbd2
saria-mohi <saria.m.mohammed@gmail.com>
2023-10-18 11:19:17 +0300

    add error message class

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,6 @@
 		wow = new WOW();
 		wow.init();
-		$(document).ready(function (e) {
-
-			$('#video-icon').on('click', function (e) {
-				e.preventDefault();
-				$('.video-popup').css('display', 'flex');
-				$('.iframe-src').slideDown();
-			});
-			$('.video-popup').on('click', function (e) {
-				var $target = e.target.nodeName;
-				var video_src = $(this).find('iframe').attr('src');
-				if ($target != 'IFRAME') {
-					$('.video-popup').fadeOut();
-					$('.iframe-src').slideUp();
-					$('.video-popup iframe').attr('src', " ");
-					$('.video-popup iframe').attr('src', video_src);
-				}
-			});
-
-			$('.slider').bxSlider({
-				pager: false
-			});
-		});
-
+	
 		$(window).on("scroll", function () {
 
 			var bodyScroll = $(window).scrollTop(),
